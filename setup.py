3df40b06589d063d7ab070d8e66a4369a5e05411
Balázs Póka <p.balazs@gmail.com>
2021-09-01 18:21:44 +0200

    release 0.3.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='igmpquerier',
-      version=0.3,
+      version='0.3.1',
       description='IGMP querier service',
       author='Marc Culler, Balázs Póka',
       author_email='marc.culler@gmail.com, p.balazs@gmail.com',
